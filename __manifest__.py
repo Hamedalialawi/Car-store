d4ae691fa11c56804c6301fcb03f8e56ef4f3f23
Hamedalialawi <137650893+Hamedalialawi@users.noreply.github.com>
2023-06-24 23:51:36 +0300

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+  'name': 'Cars',
+  'version': '1.0',
+  'category': 'Accounting/Accounting',
+  'author': 'hamed ali alhamed',
+  'website': "",
+  'company': 'hamed ali alhamed',
+  'maintainer': 'hamed ali alhamed',
+  'summary': 'Record Car Details',
+  "description": "",
+  'depends': ['base'],
+   'data': [
+      'security/cars.csv',
+      'views/cars_views.xml',
+  ],
+  'installable':True,
+  'application':True,
+  'license':'LGPL-3',
+ 
+}
